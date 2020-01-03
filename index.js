4aa405d9b621aabc9f55cd0f328f0d3f3ef961e8
alexmorris10x <alexmorris10x@icloud.com>
2020-01-02 17:20:14 -0800

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const winston = require("winston");
 const express = require("express");
 const config = require("config");
+const path = require("path");
 const app = express();
 
 require("./startup/logging")();
@@ -11,6 +12,14 @@ require("./startup/config")();
 require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
